c70aca08282bf1ed4b803ad01bf0dbd461196f5f
Mpdreamz <mpdreamz@gmail.com>
2017-12-08 14:57:07 +0100

    Introduce common base class for responses return _nodes statistics, renamed ShardsMetadata to ShardStatistics

--- src/Nest/Search/Search/SearchResponse.cs
+++ src/Nest/Search/Search/SearchResponse.cs
@@ -11,7 +11,7 @@ namespace Nest
 		/// <summary>
 		/// Gets the meta data about the shards on which the search query was executed.
 		/// </summary>
-		ShardsMetadata Shards { get; }
+		ShardStatistics Shards { get; }
 
 		/// <summary>
 		/// Gets the meta data about the hits that match the search query criteria.
@@ -110,7 +110,7 @@ namespace Nest
 		internal ServerError MultiSearchError { get; set; }
 
 		[JsonProperty("_shards")]
-		public ShardsMetadata Shards { get; internal set; }
+		public ShardStatistics Shards { get; internal set; }
 
 		[JsonProperty("aggregations")]
 		[JsonConverter(typeof(VerbatimDictionaryKeysJsonConverter<string, IAggregate>))]
